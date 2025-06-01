67c6fcc79a9b3a08242f8284c5f8c89ea62bf168
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2025-05-31 20:22:14 -0600

    started work on post page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { enhance } from '$app/forms';
+    import placeholderImage from '$lib/images/placeholder-picture.png'
+</script>
+
+<div>
+    <h3>New Post</h3>
+    <a href="/">X</a>
+    <form method="POST" use:enhance>
+        <img src={placeholderImage} alt="placeholder"/>
+    </form>
+</div>
+
+<style>
+    img {
+        width: 95%;
+        border-radius: 10px;
+        border: 5px solid black;
+    }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true,
+		}
+	},
 	test: {
 		workspace: [
 			{
