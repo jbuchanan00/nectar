d0b6496ac9d05341f984cabbe7ab39e460f49818
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2025-06-07 00:07:06 -0600

    clean up

--- src/lib/db/queries/getUser/getUserHashByEmail.ts
+++
@@ -1,9 +0,0 @@
-import type { PoolClient } from "pg";
-import { getUserIdByEmail } from "./getUserByEmail";
-
-
-export async function getUserHashByEmail(db: PoolClient, email: string): Promise<string>{
-    const userId = await getUserIdByEmail(db, email)
-    const hash = await db.query(`SELECT hash FROM authentication WHERE userId=$1`, [BigInt(userId)])
-    return hash.rows[0].hash
-}
