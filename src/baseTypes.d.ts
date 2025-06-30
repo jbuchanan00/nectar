593e9740ac2f5c21cb92bc6db224857e95ec753f
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2025-06-30 15:10:14 -0600

    continued work on post

--- src/baseTypes.d.ts
+++ src/baseTypes.d.ts
@@ -2,16 +2,17 @@ export declare type PostForm = {
     tag: string[],
     image?: Blob | string | null,
     desc?: string | null,
-    role?: string | null
+    role: 'canvas' | 'artist' | 'shop'
 }
 
 export declare type Post = {
     id: string,
     createdAt: Date | string | number,
-    updatedAt: Date | string | number,
+    updatedAt: Date | string | number | null,
     role: 'canvas' | 'artist' | 'shop',
     mediaType: "image" | "gif" | "video" | "slideshow",
     mediaId: string,
+    desc: string,
     likeCount: number
 }
 
