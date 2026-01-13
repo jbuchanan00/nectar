58f9cc4fe9ba121fe0338cb5dfb162620aed3aa4
Josh Buchanan <josh.buchanan00@gmail.com>
2026-01-12 20:34:44 -0700

    fixing the db call so it can be done with an array of inputs

--- src/lib/db/handlers/posts/getPostsByUserIds.ts
+++ src/lib/db/handlers/posts/getPostsByUserIds.ts
@@ -3,7 +3,14 @@ import type { Post } from "../../../../baseTypes";
 
 
 export async function getPostsByUserIds(db: PoolClient, users: {id: string}[]): Promise<Post[]>{
-    const query = await db.query('SELECT * FROM post WHERE user_id IN $1 ORDER BY created_at', [users])
+    let listStr = ""
+    let listNum = 1
+    const list = users.map(user => {
+        listStr += "$" + listNum + ","
+        listNum++
+        return user.id
+    })
+    const query = await db.query(`SELECT * FROM post WHERE user_id IN (${listStr.slice(0, -1)}) ORDER BY created_at`, [...list])
 
     if(query.rows.length < 1){
         console.log('No posts found')
