661cd85acda39fbb6ee1d2ca5ea1585c7d0a3d92
jbuchanan <josh.buchanan00@gmail.com>
2026-02-15 12:44:02 -0700

    changed the tag info being pulled

--- src/lib/db/handlers/tags/getTagsForPost.ts
+++ src/lib/db/handlers/tags/getTagsForPost.ts
@@ -2,7 +2,7 @@ import type { PoolClient } from "pg";
 
 
 export async function getTagsForPost(db: PoolClient, postId: string){
-    const query = "SELECT * FROM tag LEFT JOIN post_tag ON tag.id = post_tag.tag_id WHERE post_tag.post_id = $1"
+    const query = "SELECT tag.tag_name, post_tag.post_id FROM tag LEFT JOIN post_tag ON tag.id = post_tag.tag_id WHERE post_tag.post_id = $1"
 
     return await db.query(query, [postId]).then(res => {
         return res.rows
