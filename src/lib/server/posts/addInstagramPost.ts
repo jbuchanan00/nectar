a71fd6c219e79319f0ffccae171e37ffeaab61e2
Josh <josh.buchanan00@gmail.com>
2026-07-23 21:26:09 -0600

    added the server and db functions for adding instagram posts

--- src/lib/server/posts/addInstagramPost.ts
+++ src/lib/server/posts/addInstagramPost.ts
@@ -2,6 +2,17 @@ import type { PoolClient } from "pg";
 import type { InstagramPost } from "../../../baseTypes";
 
 
-export async function addInstagramPost(db: PoolClient, payload: InstagramPost, imageId: string){
-    
+export async function addInstagramPosts(db: PoolClient, payload: InstagramPost[]) {
+  const query = "INSERT INTO instagram_post (id, created_at, user_id, media_type, permalink, media_ext) VALUES "
+  let values = ""
+  let data = []
+  for (let i = 0; i < payload.length; i++) {
+    values += `(${i + 1}, ${i + 2}, ${i + 3}, ${i + 4}, ${i + 5})`
+    if (i != payload.length - 1) {
+      values += ", "
+    }
+    data.push(payload[i].id, payload[i].timestamp, payload[i].userId, payload[i].media_type, payload[i].permalink, payload[i].photoExt)
+  }
+  const res = await db.query(query + values, data);
+  return res.rows
 }
